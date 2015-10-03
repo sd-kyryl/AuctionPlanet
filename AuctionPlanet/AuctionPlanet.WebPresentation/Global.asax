6fb4fd74afabdd29422934ed74a145e9e26caef3
Kyryl Shestakov <KyrylO.Shestakov@livenau.net>
2015-10-03 14:50:39 +0300

    Created WebPresentation project
    Initial template of an ASP.NET web application with Individual User
    Accounts

---
+++ AuctionPlanet/AuctionPlanet.WebPresentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuctionPlanet.WebPresentation.MvcApplication" Language="C#" %>
---
+++ AuctionPlanet/AuctionPlanet.WebPresentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuctionPlanet.WebPresentation.Startup))]
+namespace AuctionPlanet.WebPresentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
